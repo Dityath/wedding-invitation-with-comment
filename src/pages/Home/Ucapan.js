0f490732b906d822ce28a6a1fc6d4d592d580ed0
alfi2811 <alfinasution64@gmail.com>
2021-05-09 22:19:24 +0700

    fix some section

---
+++ src/pages/Home/Ucapan.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import Slider from "react-slick";
+import pic1 from '../../assets/images/ucapan_1.png'
+import arrBack from "../../assets/images/Slider/ArrowBack.png";
+import arrNext from "../../assets/images/Slider/ArrowNext.png";
+
+function NextArrow(props) {
+    const { onClick } = props;
+    return (
+      <div
+        className="arrow-next"
+        onClick={onClick}
+      >
+          <img src={arrNext} alt="" />
+      </div>
+    );
+  }
+  
+  function PrevArrow(props) {
+    const { onClick } = props;
+    return (
+      <div
+        className="arrow-back"        
+        onClick={onClick}
+      >
+          <img src={arrBack} alt="" />
+      </div>
+    );
+  }
+  const Ucapan = () => {
+    // const ref = useRef(null);      
+    // console.log(width)  
+
+    const settings = {
+      className: "center",
+      dots: true,      
+      infinite: true,      
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,     
+      adaptiveHeight: true,   
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    };  
+    const people = [ 
+        {
+            url: pic1,
+            name: 'Afuza Imam Awed Alfaruq',
+            pesan: 'Untuk Suami, Percayalah, ketika engkau memuliakan istrimu, maka sejak saat itu kebahagiaan akan selalu menghampirimu... Untuk Istri, Suami tidak akan pernah berhenti untuk memikirkan pasangannya (istri). Percayalah, dia selalu memikirkan dan memberikan yang terbaik untuk dirimu...'
+        },
+        {
+            url: pic1,
+            name: 'Anddy Marrusliansyah',
+            pesan: 'Kami tidak bisa memberikan sesuatu yang terbaik untuk dirimu, teman sekaligus keluarga untukmu. Setelah sekian lama akhirnya dirimu menemukan tambatan hati yang terakhir, kami akan selalu berdoa semoga hal terbaik menyertai u dan mampu mengarungi bahtera keluarga yang baru'
+        },
+    ] 
+    return (
+        <div className="ucapan-container" id="gal-wed" data-aos="fade-up">            
+          <h1 className="title">Dari Sahabat</h1>            
+          {/* <div ref={ref} style={{width: '100%'}}>Hello</div> */}
+          <div className="carousel" style={{width: '80%'}} data-aos="fade-up">
+            <Slider {...settings} >
+              {
+                people.map(person => (
+                  <div >
+                      <div className="carousel-img">
+                          <img src={person.url} alt="" />
+                        <h3>{person.name}</h3>
+                        <p>"{person.pesan}"</p>
+                      </div>
+                  </div>   
+
+                ))
+              }
+                                
+            </Slider>
+          </div>          
+        </div>
+    )
+}
+
+export default Ucapan
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 // import { useSelector } from 'react-redux'
 import Aos from "aos"
 import "aos/dist/aos.css"
@@ -10,13 +10,26 @@ import Footer from './Footer'
 import Header from './Header'
 import Penutup from './Penutup'
 import Undangan from './Undangan'
+import Ucapan from './Ucapan'
 
 const Home = () => {
     // const posts = useSelector(state => state?.main?.posts)
     // console.log(posts)
     useEffect(() => {
         Aos.init({duration: 1500, once: true});
-    }, []);    
+    }, []);        
+    const cdn = "https://cdngarenanow-a.akamaihd.net/webid/FF/emerald/"
+    const [music, setMusic] = useState(false);    
+    const myRef = useRef();
+
+    const play = () => {
+        setMusic(true);
+        myRef.current.play()          
+    }
+    const pause = () => {
+        myRef.current.pause()
+        setMusic(false);                
+    }
     return (
         <div className="home-container">
             <Header />
@@ -24,6 +37,7 @@ const Home = () => {
             <Akad />
             <Dokumentasi />
             <Amplop />
+            <Ucapan />
             <Penutup />            
             <Footer />
             {/* {
@@ -31,6 +45,25 @@ const Home = () => {
                     <p>{data.title}</p>
                 ))                
             } */}            
+            <div className="btn-audio">
+                {
+                    // music?
+                    // <iframe src=""
+                    //     frameborder='0'
+                    //     allow='autoplay; encrypted-media'                            
+                    //     title='video'
+                    //     width="0"
+                    //     height="0"
+                    // /> 
+                    // :
+                    // ''
+                }   
+                <audio
+                    ref={myRef}
+                    src="https://res.cloudinary.com/bagastri07/video/upload/v1620572921/y2mate.com_-_Marry_Me_Thomas_Rhett_Lyrics_btvj9e.mp3"
+                />                
+                <img src={cdn+"img_music_on.png"} style={music?{opacity: '1'}:{opacity: '0.5'}} onClick={music? pause: play} alt="" />                
+            </div>
         </div>
     )
 }
